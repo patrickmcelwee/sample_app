e6d0b78afa32e44522056e54955a6131cde89fb3
Patrick McElwee <pmcelwee@gmail.com>
2011-12-27 23:03:43 -0500

    Created Users create action to deal with failures

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 
 end
