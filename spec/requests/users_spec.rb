36f321b915a03d7d6ad15a140c5e26d78ba1de29
Patrick McElwee <pmcelwee@gmail.com>
2011-12-29 01:32:31 -0500

    User signup complete

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -19,7 +19,23 @@ describe "Users" do
         end.should_not change(User, :count)
       end
     end
+    
+    describe "success" do
+
+      it "should make a new user" do
+        lambda do
+          visit signup_path
+          fill_in "Name",                   :with => "Example User"
+          fill_in "Email",                  :with => "user@example.com"
+          fill_in "Password",               :with => "foobar"
+          fill_in "Password confirmation",  :with => "foobar"
+          click_button
+          response.should render_template('users/show')
+          response.should have_selector("div.flash.success",
+                                        :content => "Welcome")
+        end.should change(User, :count).by(1)
+      end
+    end
   end
 
-  end
 end
